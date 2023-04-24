c490f2ab7933dc8277c8398ad7614a9ed05f931b
Burcu <burju7@yahoo.com>
2023-04-24 22:18:00 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  const capitalizedTutorials = tutorials.map((sentence) => 
+    sentence
+      .split(" ")
+      .map((word) => 
+        word[0].toUpperCase() + word.slice(1)
+      )
+      .join(" ")
+      .toString()
+  );
+
+  return capitalizedTutorials;
+};
+
+console.log(titleCased());
